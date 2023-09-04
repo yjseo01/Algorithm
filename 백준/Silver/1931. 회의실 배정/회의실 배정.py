a52bc764bdf8076beab631540bf40158775ced20
서유진 <yujin071610@gmail.com>
2023-09-04 20:26:15 +0900

    [Silver I] Title: 회의실 배정, Time: 296 ms, Memory: 55348 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+meetings = []
+n = int(input())
+for _ in range(n):
+    meetings.append(tuple(map(int, input().rsplit())))
+
+meetings.sort(key = lambda x: (x[1], -x[0]))
+
+last = meetings[0]
+if last[0] == last[1]:
+    cnt = 0
+else:
+    cnt = 1
+
+for i in meetings:
+    if last[1] <= i[0]:
+        cnt += 1
+        last = i
+    elif last[0] >= i[1]:
+        cnt += 1
+        last = i
+
+print(cnt)
