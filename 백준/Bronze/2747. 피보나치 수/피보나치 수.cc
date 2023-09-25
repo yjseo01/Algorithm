d3a6155aeccbc4bd4e77d2f921d8a9f3f0223fa1
서유진 <yujin071610@gmail.com>
2023-09-25 15:51:36 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> vec;
+    vec.reserve(n + 1);
+
+    vec[0] = 0;
+    vec[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        vec[i] = vec[i - 1] + vec[i - 2];
+    }
+
+    cout << vec[n] << "\n";
+    return 0;
+}
