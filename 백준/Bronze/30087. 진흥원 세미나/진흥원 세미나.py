580bb2cdc39ad2e5c2c6fb87a23b4e3870c0b094
서유진 <yujin071610@gmail.com>
2024-08-15 23:55:22 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,11 @@
+dict = {}
+dict['Algorithm'] = '204'
+dict['DataAnalysis'] = '207'
+dict['ArtificialIntelligence'] = '302'
+dict['CyberSecurity'] = 'B101'
+dict['Network'] = '303'
+dict['Startup'] = '501'
+dict['TestStrategy'] = '105'
+
+for _ in range(int(input())):
+    print(dict[input()])
