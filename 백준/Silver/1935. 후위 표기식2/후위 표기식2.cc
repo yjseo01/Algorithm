ce7e8b0905d5a148b3e485b15db2fba805db165a
Yujin Seo <yujin071610@gmail.com>
2025-03-31 10:45:31 +0900

    [Silver III] Title: 후위 표기식2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	string postfix;
+	vector<double> stack;
+	cin >> n >> postfix;
+	vector<int> vals(n, 0);
+	for (int i = 0; i < n; i++)
+		cin >> vals[i];
+	k = postfix.length();
+
+	double x, y;
+	for (char c : postfix)
+	{
+		if (65 <= c && c <= 90) // ABC...Z
+		{
+			stack.push_back((double)vals[c - 65]);
+		}
+		else // +-*/
+		{
+			y = stack.back();
+			stack.pop_back();
+			x = stack.back();
+			stack.pop_back();
+
+			switch (c)
+			{
+			case '+':
+				stack.push_back(x + y);
+				break;
+			case '-':
+				stack.push_back(x - y);
+				break;
+			case '*':
+				stack.push_back(x * y);
+				break;
+			case '/':
+				stack.push_back(x / y);
+			}
+		}
+	}
+
+	//cout << stack.back() << "\n";
+	cout << fixed << setprecision(2) << stack.back() << "\n";
+
+	return 0;
+}
