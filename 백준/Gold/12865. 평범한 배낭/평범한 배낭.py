7902982b4a6217a4c65127fc6cc0d11ce8d834d3
Yujin Seo <yujin071610@gmail.com>
2025-05-17 14:43:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 4788 ms, Memory: 230376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,20 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
 w = [0] * (n + 1)
 v = [0] * (n + 1)
-
 dp = [[0] * (k + 1) for _ in range(n + 1)]
-
 for i in range(1, n + 1):
     w[i], v[i] = map(int, input().split())
 
 for i in range(1, n + 1):
     for j in range(1, k + 1):
-        if j >= w[i]:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
-        else:
+        if j >= w[i]: # i번째 물건을 넣을 수 있음
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]) # max(i번째 물건을 넣지 않는 경우, i번째 물건을 넣는 경우)
+        else: # i번째 물건을 넣을 수 없음
             dp[i][j] = dp[i - 1][j]
 
 print(dp[n][k])
