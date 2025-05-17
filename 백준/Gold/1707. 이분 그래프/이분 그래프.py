57ba0627a5985265824a05ca25e297a3070e96ff
Yujin Seo <yujin071610@gmail.com>
2025-05-17 12:58:40 +0900

    [Gold IV] Title: 이분 그래프, Time: 1140 ms, Memory: 53996 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,39 +1,45 @@
 import sys
+sys.setrecursionlimit(20005)
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(start, graph, visited):
-    queue = deque([start])
+def bfs(graph, visited, start):
+    queue = deque()
+    queue.append(start)
     visited[start] = 0
 
     while queue:
         x = queue.popleft()
         for nx in graph[x]:
-            if visited[nx] == -1:
-                visited[nx] = 1 - visited[x]
+            if visited[nx] != -1:
+                if visited[nx] == visited[x]:
+                    return False
+            else:
                 queue.append(nx)
-            elif visited[nx] == visited[x]:
-                return False
-            
+                visited[nx] = 1 - visited[x]
+
     return True
 
 k = int(input())
-for _ in range(k):
-    v, e = map(int, input().split())
-    graph = [[] for _ in range(v + 1)]
+while k > 0:
+    k -= 1
+    
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V + 1)]
 
-    for _ in range(e):
-        x, y = map(int, input().split())
-        graph[x].append(y)
-        graph[y].append(x)
+    for _ in range(E):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
 
-    visited = [-1] * (v + 1)
-    is_true = True
-
-    for i in range(1, v + 1):
+    visited = [-1] * (V + 1)
+    isTrue = True
+    for i in range(1, V + 1):
         if visited[i] == -1:
-            if not bfs(i, graph, visited):
-                is_true = False
+            if not bfs(graph, visited, i):
+                print("NO")
+                isTrue = False
                 break
-        
-    print("YES" if is_true else "NO")
+
+    if isTrue:
+        print("YES")
